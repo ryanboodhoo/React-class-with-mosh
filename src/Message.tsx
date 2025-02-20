362b5639e546dd769aaca0b0d98e641a967ab665
ryanboodhoo <boodho46@gmail.com>
2025-02-20 17:51:06 -0500

    done with this for now

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+//Pascal Casing
+function Message (){
+    //JSX :Javascript XML
+    const name = "Ryan";
+if (name)
+    return <h1>Hello {name}</h1>;
+else
+return <h1>Hello User</h1>;
+
+}
+export default Message; //Exporting the function
